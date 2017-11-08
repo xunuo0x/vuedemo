791cb2ad58dffb3326d458d005a873181e3ce735
xunuo0x <1175176908@qq.com>
2017-11-08 12:22:46 +0800

    atom update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="index-wrap">
-    <div class="index-left">
-      <div class="index-left-block">
-        <h2>全部产品</h2>
-        <template v-for="product in productList">
+<div class="index-wrap">
+  <div class="index-left">
+    <div class="index-left-block">
+      <h2>全部产品</h2>
+      <template v-for="product in productList">
           <h3>{{product.title}}</h3>
           <ul v-for="item in product.list">
             <li>
@@ -12,267 +12,262 @@
           </ul>
           <div class="hr"></div>
         </template>
-      </div>
-      <div class="index-left-block">
-        <h2>最新消息</h2>
-        <ul v-for="item in newsList">
-          <li>
-            {{item.product}}
-          </li>
-        </ul>
-      </div>
     </div>
-    <div class="index-right">
-      <div class="index-board-list">
-        <div v-for="(item,index) in boardList" class="index-board-item"
-             :class="[{'line-last' : index %2 !== 0},'index-board-' + item.id ]">
-          <div class="index-board-item-inner">
-            <h2>{{item.title}}</h2>
-            <p>{{item.description}}</p>
-            <div class="index-board-button">
-              <a href="" class="button">立即购买</a>
-            </div>
+    <div class="index-left-block">
+      <h2>最新消息</h2>
+      <ul v-for="item in newsList">
+        <li>
+          {{item.product}}
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="index-right">
+    <div class="index-board-list">
+      <div v-for="(item,index) in boardList" class="index-board-item" :class="[{'line-last' : index %2 !== 0},'index-board-' + item.id ]">
+        <div class="index-board-item-inner">
+          <h2>{{item.title}}</h2>
+          <p>{{item.description}}</p>
+          <div class="index-board-button">
+            <a href="" class="button">立即购买</a>
           </div>
         </div>
       </div>
     </div>
   </div>
+</div>
 </template>
 <script type="text/ecmascript-6">
-  export default {
-    created () {
-      this.$http.get('getList')
-        .then(function (data) {
-          console.log(data)
-        }, function (err) {
-          console.log(err)
-        })
-    },
-    data () {
-      return {
-        boardList: [
-          {
-            title: '开放产品',
-            description: '开放产品是一款开放产品',
-            id: 'car',
-            toKey: 'analysis',
-            saleout: false
-          },
-          {
-            title: '品牌营销',
-            description: '品牌营销帮助你的产品更好地找到定位',
-            id: 'earth',
-            toKey: 'count',
-            saleout: false
-          },
-          {
-            title: '使命必达',
-            description: '使命必达快速迭代永远保持最前端的速度',
-            id: 'loud',
-            toKey: 'forecast',
-            saleout: true
-          },
-          {
-            title: '勇攀高峰',
-            description: '帮你勇闯高峰，到达事业的顶峰',
-            id: 'hill',
-            toKey: 'publish',
-            saleout: false
-          }
-        ],
-        newsList: [
-          {
-            'orderId': 'ddj123',
-            'product': '数据统计',
-            'version': '高级版',
-            'period': '1年',
-            'buyNum': 2,
-            'date': '2016-10-10',
-            'amount': '500元'
-          },
-          {
-            'orderId': 'yuj583',
-            'product': '流量分析',
-            'version': '户外版',
-            'period': '3个月',
-            'buyNum': 1,
-            'date': '2016-5-2',
-            'amount': '2200元'
-          },
-          {
-            'orderId': 'pmd201',
-            'product': '广告发布',
-            'version': '商铺版',
-            'period': '3年',
-            'buyNum': 12,
-            'date': '2016-8-3',
-            'amount': '7890元'
-          }
-        ],
-        productList: {
-          pc: {
-            title: 'PC产品',
-            list: [
-              {
-                name: '数据统计',
-                url: 'http://starcraft.com'
-              },
-              {
-                name: '数据预测',
-                url: 'http://warcraft.com'
-              },
-              {
-                name: '流量分析',
-                url: 'http://overwatch.com',
-                hot: true
-              },
-              {
-                name: '广告发布',
-                url: 'http://hearstone.com'
-              }
-            ]
-          },
-          app: {
-            title: '手机应用类',
-            last: true,
-            list: [
-              {
-                name: '91助手',
-                url: 'http://weixin.com'
-              },
-              {
-                name: '产品助手',
-                url: 'http://twitter.com',
-                hot: true
-              },
-              {
-                name: '智能地图',
-                url: 'http://maps.com'
-              },
-              {
-                name: '团队语音',
-                url: 'http://phone.com'
-              }
-            ]
-          }
+export default {
+  created() {
+    this.$http.get('getList')
+      .then(function(data) {
+        console.log(data)
+      }, function(err) {
+        console.log(err)
+      })
+  },
+  data() {
+    return {
+      boardList: [{
+          title: '开放产品',
+          description: '开放产品是一款开放产品',
+          id: 'car',
+          toKey: 'analysis',
+          saleout: false
+        },
+        {
+          title: '品牌营销',
+          description: '品牌营销帮助你的产品更好地找到定位',
+          id: 'earth',
+          toKey: 'count',
+          saleout: false
+        },
+        {
+          title: '使命必达',
+          description: '使命必达快速迭代永远保持最前端的速度',
+          id: 'loud',
+          toKey: 'forecast',
+          saleout: true
+        },
+        {
+          title: '勇攀高峰',
+          description: '帮你勇闯高峰，到达事业的顶峰',
+          id: 'hill',
+          toKey: 'publish',
+          saleout: false
+        }
+      ],
+      newsList: [{
+          'orderId': 'ddj123',
+          'product': '数据统计',
+          'version': '高级版',
+          'period': '1年',
+          'buyNum': 2,
+          'date': '2016-10-10',
+          'amount': '500元'
+        },
+        {
+          'orderId': 'yuj583',
+          'product': '流量分析',
+          'version': '户外版',
+          'period': '3个月',
+          'buyNum': 1,
+          'date': '2016-5-2',
+          'amount': '2200元'
+        },
+        {
+          'orderId': 'pmd201',
+          'product': '广告发布',
+          'version': '商铺版',
+          'period': '3年',
+          'buyNum': 12,
+          'date': '2016-8-3',
+          'amount': '7890元'
+        }
+      ],
+      productList: {
+        pc: {
+          title: 'PC产品',
+          list: [{
+              name: '数据统计',
+              url: 'http://starcraft.com'
+            },
+            {
+              name: '数据预测',
+              url: 'http://warcraft.com'
+            },
+            {
+              name: '流量分析',
+              url: 'http://overwatch.com',
+              hot: true
+            },
+            {
+              name: '广告发布',
+              url: 'http://hearstone.com'
+            }
+          ]
+        },
+        app: {
+          title: '手机应用类',
+          last: true,
+          list: [{
+              name: '91助手',
+              url: 'http://weixin.com'
+            },
+            {
+              name: '产品助手',
+              url: 'http://twitter.com',
+              hot: true
+            },
+            {
+              name: '智能地图',
+              url: 'http://maps.com'
+            },
+            {
+              name: '团队语音',
+              url: 'http://phone.com'
+            }
+          ]
         }
       }
     }
   }
+}
 </script>
 <style scoped>
-  .index-wrap {
-    width: 1200px;
-    margin: 0 auto;
-    overflow: hidden;
-  }
+.index-wrap {
+  width: 1200px;
+  margin: 0 auto;
+  overflow: hidden;
+}
 
-  .index-left {
-    float: left;
-    width: 300px;
-    text-align: left;
-  }
+.index-left {
+  float: left;
+  width: 300px;
+  text-align: left;
+}
 
-  .index-right {
-    float: left;
-    width: 900px;
-  }
+.index-right {
+  float: left;
+  width: 900px;
+}
 
-  .index-left-block {
-    margin: 15px;
-    background: #fff;
-    box-shadow: 0 0 1px #ddd;
-  }
+.index-left-block {
+  margin: 15px;
+  background: #fff;
+  box-shadow: 0 0 1px #ddd;
+}
 
-  .index-left-block .hr {
-    margin-bottom: 20px;
-  }
+.index-left-block .hr {
+  margin-bottom: 20px;
+}
 
-  .index-left-block h2 {
-    background: #4fc08d;
-    color: #fff;
-    padding: 10px 15px;
-    margin-bottom: 20px;
-  }
+.index-left-block h2 {
+  background: #4fc08d;
+  color: #fff;
+  padding: 10px 15px;
+  margin-bottom: 20px;
+}
 
-  .index-left-block h3 {
-    padding: 0 15px 5px 15px;
-    font-weight: bold;
-    color: #222;
-  }
+.index-left-block h3 {
+  padding: 0 15px 5px 15px;
+  font-weight: bold;
+  color: #222;
+}
 
-  .index-left-block ul {
-    padding: 10px 15px;
-  }
+.index-left-block ul {
+  padding: 10px 15px;
+}
 
-  .index-left-block li {
-    padding: 5px;
-  }
+.index-left-block li {
+  padding: 5px;
+}
 
-  .index-board-list {
-    overflow: hidden;
-  }
+.index-board-list {
+  overflow: hidden;
+}
 
-  .index-board-item {
-    float: left;
-    width: 400px;
-    background: #fff;
-    box-shadow: 0 0 1px #ddd;
-    padding: 20px;
-    margin-right: 20px;
-    margin-bottom: 20px;
-  }
+.index-board-item {
+  float: left;
+  width: 400px;
+  background: #fff;
+  box-shadow: 0 0 1px #ddd;
+  padding: 20px;
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
 
-  .index-board-item-inner {
-    min-height: 125px;
-    padding-left: 120px;
-  }
+.index-board-item-inner {
+  min-height: 125px;
+  padding-left: 120px;
+}
 
-  .index-board-car .index-board-item-inner {
-    background: url(../assets/images/1.png) no-repeat;
-  }
+.index-board-car .index-board-item-inner {
+  background: url(../assets/images/1.png) no-repeat;
+}
 
-  .index-board-loud .index-board-item-inner {
-    background: url(../assets/images/2.png) no-repeat;
-  }
+.index-board-loud .index-board-item-inner {
+  background: url(../assets/images/2.png) no-repeat;
+}
 
-  .index-board-earth .index-board-item-inner {
-    background: url(../assets/images/3.png) no-repeat;
-  }
+.index-board-earth .index-board-item-inner {
+  background: url(../assets/images/3.png) no-repeat;
+}
 
-  .index-board-hill .index-board-item-inner {
-    background: url(../assets/images/4.png) no-repeat;
-  }
+.index-board-hill .index-board-item-inner {
+  background: url(../assets/images/4.png) no-repeat;
+}
 
-  .index-board-item h2 {
-    font-size: 18px;
-    font-weight: bold;
-    color: #000;
-    margin-bottom: 15px;
-  }
+.index-board-item h2 {
+  font-size: 18px;
+  font-weight: bold;
+  color: #000;
+  margin-bottom: 15px;
+}
 
-  .line-last {
-    margin-right: 0;
-  }
+.line-last {
+  margin-right: 0;
+}
 
-  .index-board-button {
-    margin-top: 20px;
-  }
+.index-board-button {
+  margin-top: 20px;
+}
 
-  .lastest-news {
-    min-height: 512px;
-  }
+.lastest-news {
+  min-height: 512px;
+}
 
-  .hot-tag {
-    background: red;
-    color: #fff;
-  }
+.hot-tag {
+  background: red;
+  color: #fff;
+}
 
-  .new-item {
-    display: inline-block;
-    width: 230px;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  }
+.new-item {
+  display: inline-block;
+  width: 230px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
 </style>
